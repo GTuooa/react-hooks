bc0fbda3cea63163490d53905febc97d033ab200
GTuooa <1659256785@qq.com>
2020-11-16 23:21:46 +0800

    Feat: 新增hooks

---
+++ hooks/src/UseState.js
@@ -0,0 +1,14 @@
+import React, { Component, useState } from 'react'
+
+export default function UseState () {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>You click {count} times</div>
+      <button onClick={() => { setCount(count + 1) }}>点击+1</button>
+    </>
+  )
+
+}
---
+++ hooks/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import UseState from './UseState.js'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UseState />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
