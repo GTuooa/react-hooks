e275f1681b397c399ccec74b117e043d1c4972fe
GTuooa <1659256785@qq.com>
2020-11-22 15:25:30 +0800

    Feat： 嵌套路由

--- hooks/src/UseState.js
+++ hooks/src/UseState.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Route, Link } from 'react-router-dom'
+
 
 
 export default function UseState (props) {
@@ -11,6 +12,19 @@ export default function UseState (props) {
 
   return (
     <>
+      <ul>
+        <li><Link to='/use-state/about'>useState简介</Link></li>
+        <li><Link to='/use-state/overview'>useState概览</Link></li>
+        <li><Link to='/use-state/use'>useState使用</Link></li>
+      </ul>
+      <hr></hr>
+
+      <Route path='/use-state/about' component={About} />
+      <Route path='/use-state/overview' component={Overview} />
+      <Route path='/use-state/use' component={Use} />
+
+
+      <hr></hr>
       <h2>useState</h2>
       <div>You click {count} times</div>
       <button onClick={() => { setCount(count + 1) }}>点击+1</button>
@@ -18,4 +32,22 @@ export default function UseState (props) {
     </>
   )
 
+}
+
+function About () {
+  return (
+    <h2>我是useState简介页面</h2>
+  )
+}
+
+function Overview () {
+  return (
+    <h2>我是useState概览页面</h2>
+  )
+}
+
+function Use () {
+  return (
+    <h2>我是useState使用页面</h2>
+  )
 }
