5f6249142074cbab8a471a454da1ff6f16731cbb
GTuooa <1659256785@qq.com>
2021-03-16 14:13:38 +0800

    feat： 添加IntersectionObserver demo

--- hooks/src/UseState.js
+++ hooks/src/UseState.js
@@ -1,14 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { useHistory, Route, Link } from 'react-router-dom'
-
-
 
 export default function UseState (props) {
 
   const [count, setCount] = useState(0)
+  const [list, setList] = useState([0, 0, 0, 0, 0])
+  const [page, setPage] = useState(1)
   const history = useHistory()
-  console.log(history)
-  console.log(props)
+  // console.log(history)
+  // console.log(props)
+  console.log("render", page, list)
+
+  const divRef = useRef()
+
+  useEffect(() => {
+    // http://www.ruanyifeng.com/blog/2016/11/intersectionobserver_api.html
+    const io = new IntersectionObserver((entries) => {
+        if (entries[0]['isIntersecting']) { // 当前元素可见
+          setPage((page) => {
+            if(page < 5){
+              setTimeout(() => {
+                setList((list) => list.concat([page, page, page, page, page]))
+              }, 1000)
+              return page+1
+            } else {
+              return page
+            }
+          })
+        }
+    })
+    io.observe(divRef.current)
+    return () => io.disconnect()
+  }, [])
 
   return (
     <>
@@ -29,6 +52,14 @@ export default function UseState (props) {
       <div>You click {count} times</div>
       <button onClick={() => { setCount(count + 1) }}>点击+1</button>
       <div onClick={() => history.push('/')}>history</div>
+
+      <div className="box">
+        {list.map((v, i) => {
+          return (<div key={i} className="list">{v}</div>)
+        })}
+        <span ref={divRef}>加载更多</span>
+
+      </div>
     </>
   )
 
